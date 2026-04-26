03e813231ec4751f9ef1c866257ede2cfb30968f
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-26 15:09:00 +0700

    Update vector.lua

--- scripts/vector.lua
+++ scripts/vector.lua
@@ -10,7 +10,9 @@ function vector_scale(a,n)
  return {a[1]*n,a[2]*n,a[3]*n}
 end
 
-function Distance(a,b) return math.sqrt((a[1]-b[1])^2+(a[2]-b[2])^2+(a[3]-b[3])^2) end
+function vector_distance(a,b)
+ return math.sqrt((a[1]-b[1])^2+(a[2]-b[2])^2+(a[3]-b[3])^2)
+end
 function Dot(a, b) return a[1]*b[1] + a[2]*b[2] + a[3]*b[3] end
 function Normalised(a) local l = math.sqrt(a[1]^2 + a[2]^2 + a[3]^2) return {a[1]/l, a[2]/l, a[3]/l} end
 function Magnitude(a) return math.sqrt(a[1]^2+a[2]^2+a[3]^2) end
