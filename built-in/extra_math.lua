929e76377b8fb5348062682e73dce9384cd646f2
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-26 17:00:45 +0700

    Update extra_math.lua

--- built-in/extra_math.lua
+++ built-in/extra_math.lua
@@ -1,7 +1,14 @@
 extra_math_epsilon=1e-9
 
-function extra_math_is_zero_approx(a)
- if a>=-extra_math_epsilon and a<=extra_math_epsilon then
+func extra_math_is_approx(value,target)
+ if value>=-target and value<=target then
+  return true
+ end
+ return false
+end
+
+function extra_math_is_zero_approx(value)
+ if extra_math_is_approx(value,extra_math_epsilon) then
   return true
  end
  return false
