52997bae4c2bd40a6790d73bacf84f8232589b3f
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-26 15:48:44 +0700

    Update extra_math.lua

--- scripts/extra_math.lua
+++ scripts/extra_math.lua
@@ -1,9 +1,19 @@
-function extra_math_sign(a)
+function extra_math_sign_int(a)
  if a==0 then
   return 0
  elseif a>0 then
   return 1
  else
   return -1
+ end
+end
+
+function extra_math_sign_float(a)
+ if a==0.0 then
+  return 0.0
+ elseif a>0.0 then
+  return 1.0
+ else
+  return -1.0
  end
 end
