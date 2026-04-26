e892095fa978455cf9a21ff1efcd4eee1fb19472
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-26 15:53:46 +0700

    Update extra_math.lua

--- scripts/extra_math.lua
+++ scripts/extra_math.lua
@@ -1,19 +1,28 @@
+epsilon=1e9
+
+function extra_math_is_zero(a)
+ if a>-epsilon and a<epsilon:
+  return true
+ end
+ return false
+end
+
 function extra_math_sign_int(a)
  if a==0 then
   return 0
- elseif a>0 then
-  return 1
- else
-  return -1
  end
+ if a>0 then
+  return 1
+ end
+ return -1
 end
 
 function extra_math_sign_float(a)
  if a==0.0 then
   return 0.0
- elseif a>0.0 then
-  return 1.0
- else
-  return -1.0
  end
+ if a>0.0 then
+  return 1.0
+ end
+ return -1.0
 end
