1f8384bc3393277947884db17cdc722a4c66172d
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-26 16:26:29 +0700

    Update handheld.lua

--- scripts/handheld.lua
+++ scripts/handheld.lua
@@ -4,12 +4,12 @@ function handheld_tick()
  if!rb.IsKinematic then
   rb.IsKinematic=true
  local a=pl.CameraDirection
- local targRot=q.Multiply(q.LookRotation(a,{0,1,0}),q.Inverse(tr.LocalRotation))
- local targPos=vector_add(pl.CameraPosition,vector_scale(a,handheld_distance))
- local offsetVec=vector_subtract(mb.Position,rb.Position)
- local finalRotation=q.Multiply(targRot,q.Inverse(rb.Rotation))
- local newOffsetVec=q.MultiplyByVector(finalRotation,offsetVec)
- local offsetPos=SubtractVectors(targPos, newOffsetVec)	
- rb.Rotation=targRot
- rb.Position=offsetPos
+ local b=q.Multiply(q.LookRotation(a,{0,1,0}),q.Inverse(tr.LocalRotation))
+ local c=vector_add(pl.CameraPosition,vector_scale(a,handheld_distance))
+ local d=vector_subtract(mb.Position,rb.Position)
+ local e=q.Multiply(b,q.Inverse(rb.Rotation))
+ local f=q.MultiplyByVector(e,d)
+ local g=SubtractVectors(c,f)	
+ rb.Rotation=b
+ rb.Position=g
 end
