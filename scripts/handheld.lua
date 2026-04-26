5364252c476fdacf47f05d01747dead17cb0c253
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-26 16:17:18 +0700

    Update handheld.lua

--- scripts/handheld.lua
+++ scripts/handheld.lua
@@ -6,7 +6,7 @@ function handheld_tick()
  local dir=pl.CameraDirection
  local targRot=q.Multiply(q.LookRotation(dir,{0,1,0}),q.Inverse(tr.LocalRotation))
  local targPos=vector_add(pl.CameraPosition,vector_scale(dir,distance_from_player))
- local offsetVec=vector_subtract(mb.Position, rb.Position)
+ local offsetVec=vector_subtract(mb.Position,rb.Position)
  local finalRotation=q.Multiply(targRot,q.Inverse(rb.Rotation))
  local newOffsetVec=q.MultiplyByVector(finalRotation,offsetVec)
  local offsetPos=SubtractVectors(targPos, newOffsetVec)	
