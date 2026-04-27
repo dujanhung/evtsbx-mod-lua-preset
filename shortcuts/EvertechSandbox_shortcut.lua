547b92c0b5709ccbf658ac02b7bb061efd02c5eb
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-27 10:26:37 +0700

    Update EvertechSandbox_shortcut.lua

--- shortcuts/EvertechSandbox_shortcut.lua
+++ shortcuts/EvertechSandbox_shortcut.lua
@@ -2,11 +2,11 @@ module(
  "EvertechSandbox_shortcut"
 )
 
-EvertechSandbox_shortcut::mb=es.MultiBlock
-EvertechSandbox_shortcut::block_type=mb.Type
-EvertechSandbox_shortcut::mod_block_uuid=mb.ModBlock.Uuid
-EvertechSandbox_shortcut::rb=mb.Rigidbody
-EvertechSandbox_shortcut::tr=mb.Transform
-EvertechSandbox_shortcut::r=es.Root
-EvertechSandbox_shortcut::pl=es.Player
-EvertechSandbox_shortcut::q=es.Quaternion
+EvertechSandbox_shortcut.mb=es.MultiBlock
+EvertechSandbox_shortcut.block_type=mb.Type
+EvertechSandbox_shortcut.mod_block_uuid=mb.ModBlock.Uuid
+EvertechSandbox_shortcut.rb=mb.Rigidbody
+EvertechSandbox_shortcut.tr=mb.Transform
+EvertechSandbox_shortcut.r=es.Root
+EvertechSandbox_shortcut.pl=es.Player
+EvertechSandbox_shortcut.q=es.Quaternion
