65e6e68ccfc88ff0ab32279bd57935a816d6370b
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-27 10:40:59 +0700

    Update extra_math.lua

--- built-in/extra_math.lua
+++ built-in/extra_math.lua
@@ -1,6 +1,10 @@
-extra_math_epsilon=1e-9
+module(
+ "extra_math"
+)
 
-function extra_math_is_approx(value,target,artifact)
+extra_math.epsilon=1e-9
+
+function extra_math.is_approx(value,target,artifact)
  local o=value-target
  if o>=-artifact and o<=artifact then
   return true
@@ -8,14 +12,14 @@ function extra_math_is_approx(value,target,artifact)
  return false
 end
 
-function extra_math_is_zero_approx(value)
- if extra_math_is_approx(value,0,extra_math_epsilon) then
+function extra_math.is_zero_approx(value)
+ if extra_math.is_approx(value,0,extra_math_epsilon) then
   return true
  end
  return false
 end
 
-function extra_math_sign_int(value)
+function extra_math.sign(value)
  if value==0 then
   return 0
  end
@@ -23,14 +27,4 @@ function extra_math_sign_int(value)
   return 1
  end
  return -1
-end
-
-function extra_math_sign_float(value)
- if extra_math_is_zero_approx(value) then
-  return 0.0
- end
- if value>0.0 then
-  return 1.0
- end
- return -1.0
 end
