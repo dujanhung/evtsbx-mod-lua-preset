acbe8dadbc4b6ba30c8f9815c0d77c40717d9d0d
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-27 10:06:57 +0700

    Update handheld.lua

--- headers/handheld.lua
+++ headers/handheld.lua
@@ -1,3 +1,15 @@
+/*
+
+required decencies:
+
+https://dujanhung.github.com/extra-lua-preset/blob/safeguards/extra_math.lua
+
+https://dujanhung.github.com/extra-lua-preset/blob/variants/vector.lua
+
+https://dujanhung.github.com/evtsbx-mod-lua-preset/blob/shortcuts/EvertechSandbox_shortcut.lua
+
+*/
+
 dofile(
  "extra-lua-preset/safeguards/extra_math.lua"
 )
