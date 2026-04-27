fe2d94e7965f919ad9e08782346fe359dbc2e20a
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-27 09:29:21 +0700

    Update handheld.lua

--- preset/handheld.lua
+++ preset/handheld.lua
@@ -1,5 +1,5 @@
-dofile("scripts/evtsbx-mod-lua-preset/extra-lua-preset/scripts/vector.lua")
-dofile("scripts/evtsbx-mod-lua-preset/scripts/shortcut.lua")
+dofile("../extra-lua-preset/scripts/vector.lua")
+dofile("../scripts/shortcut.lua")
 required("vector")
 required("EvertechSandbox_shortcut")
 
