56117a6467a4d29359aa6d335198487b87ddf8f0
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-28 15:47:30 +0700

    Update EvertechSandbox_shortcut.lua

--- shortcuts/EvertechSandbox_shortcut.lua
+++ shortcuts/EvertechSandbox_shortcut.lua
@@ -1,9 +1,9 @@
 required("es")
 mb=es.MultiBlock
-mod_block_type=mb.Type
-mod_block_uuid=mb.ModBlock.Uuid
 rb=mb.Rigidbody
 tr=mb.Transform
+mod_block_type=mb.Type
+mod_block_uuid=mb.ModBlock.Uuid
 r=es.Root
 pl=es.Player
 q=es.Quaternion
