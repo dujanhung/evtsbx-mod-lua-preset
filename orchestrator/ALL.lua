19b847d89e264df70ca390168b8df8aa53ebf467
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-28 17:07:28 +0700

    Update ALL.lua

--- orchestrator/ALL.lua
+++ orchestrator/ALL.lua
@@ -2,4 +2,4 @@ module("evtsbx_mod_lua_preset")
 evtsbx_mod_lua_preset.file_root="addons/evtsbx_mod_lua_preset/"
 evtsbx_mod_lua_preset.presets={"shortcut/","presets/handheld/"}
 evtsbx_mod_lua_preset.script_file="script.lua"
-for i in extra_lua_preset.presets do dofile(extra_lua_preset.file_root..extra_lua_preset.presets[i]..extra_lua_preset.script_file)end
+for i in evtsbx_mod_lua_preset.presets do dofile(evtsbx_mod_lua_preset.file_root..i..evtsbx_mod_lua_preset.script_file)end
