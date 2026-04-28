7ce62902dc2ee896a069e747fd2ab1c72962d647
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-04-28 16:02:22 +0700

    Update handheld.lua

--- presets/handheld.lua
+++ presets/handheld.lua
@@ -3,4 +3,4 @@ required("EvertechSandbox_shortcut")
 module("EvertechSandbox_preset_handheld")
 EvertechSandbox_preset_handheld.distance=5.0
 function fixedUpdate()EvertechSandbox_preset_handheld.tick()end
-function EvertechSandbox_preset_handheld.tick()if!rb then return end if!rb.IsKinematic then rb.IsKinematic=true end local a=pl.CameraDirection;local b=q.Multiply(q.LookRotation(a,{0.0,1.0,0.0}),q.Inverse(tr.LocalRotation));rb.Rotation=b;rb.Position=vector.subtract(vector.add(pl.CameraPosition,vector.scale(a,EvertechSandbox_preset_handheld.distance)),q.MultiplyByVector(q.Multiply(b,q.Inverse(rb.Rotation)),vector.subtract(mb.Position,rb.Position)))end
+function EvertechSandbox_preset_handheld.tick()if!rb then return end if!rb.IsKinematic then rb.IsKinematic=true end local b=q.Multiply(q.LookRotation(cd,{0.0,1.0,0.0}),q.Inverse(tr.LocalRotation));rb.Rotation=b;rb.Position=vector.subtract(vector.add(pl.CameraPosition,vector.scale(cd,EvertechSandbox_preset_handheld.distance)),q.MultiplyByVector(q.Multiply(b,q.Inverse(rb.Rotation)),vector.subtract(mb.Position,rb.Position)))end
