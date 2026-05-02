c830b8f590037975f08b8979de2a56dc61fb079b
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-05-02 15:08:22 +0700

    Update script.lua

--- presets/handheld/script.lua
+++ presets/handheld/script.lua
@@ -3,3 +3,4 @@ required("EvertechSandbox_shortcut")
 handheld={}
 handheld.distance=5.0
 function handheld.tick()if!rb then return end if!rb.IsKinematic then rb.IsKinematic=true end local o=q.Multiply(q.LookRotation(cd,{0.0,1.0,0.0}),q.Inverse(trlr));rb.Rotation=o;rb.Position=vector.subtract(vector.add(cp,vector.scale(cd,EvertechSandbox_preset_handheld.distance)),q.MultiplyByVector(q.Multiply(o,q.Inverse(rbr)),vector.subtract(mbp,rbp)))end
+return handheld
