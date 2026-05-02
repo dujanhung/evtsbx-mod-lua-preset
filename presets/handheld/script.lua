bf83a51e9ad5400bd8c859db51f203b84f814bf3
Dương Chấn Hưng <chanhung2008.duong@gmail.com>
2026-05-02 14:57:44 +0700

    Update script.lua

--- presets/handheld/script.lua
+++ presets/handheld/script.lua
@@ -1,6 +1,7 @@
 required("extra-lua-preset")
 required("EvertechSandbox_shortcut")
-module("EvertechSandbox_preset_handheld")
+local EvertechSandbox_preset_handheld={}
 EvertechSandbox_preset_handheld.distance=5.0
 function fixedUpdate()EvertechSandbox_preset_handheld.tick()end
 function EvertechSandbox_preset_handheld.tick()if!rb then return end if!rb.IsKinematic then rb.IsKinematic=true end local o=q.Multiply(q.LookRotation(cd,{0.0,1.0,0.0}),q.Inverse(trlr));rb.Rotation=o;rb.Position=vector.subtract(vector.add(cp,vector.scale(cd,EvertechSandbox_preset_handheld.distance)),q.MultiplyByVector(q.Multiply(o,q.Inverse(rbr)),vector.subtract(mbp,rbp)))end
+return EvertechSandbox_preset_handheld
